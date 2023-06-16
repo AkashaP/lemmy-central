80ccbd8c7668b705fc91b00d1a1f1b5b517b5f15
Akasha Peppermint <tehrandommagik@gmail.com>
2023-06-17 07:03:03 +1000

    1

--- lemmy-central.asd
+++ lemmy-central.asd
@@ -1,9 +1,6 @@
 (defsystem #:lemmy-central
   :author "Akasha Peppermint"
   :depends-on (#:cl-html-parse #:hunchentoot #:com.inuoe.jzon #:drakma #:cl-naive-dom #:cl-naive-dom.abt #:str)
-  :local-nicknames ((:nd #:cl-naive-dom)
-                    (:json #:com.inuoe.jzon)
-                    (:time #:local-time)
-                    (:nd.abt #:cl-naive-dom.abt))
   :serial t
-  :components ((:file "lemmy-central")))
+  :components ((:file "package")
+               (:file "lemmy-central")))
---
+++ package.lisp
@@ -0,0 +1,31 @@
+(defpackage :lemmy-central
+  (:use :cl)
+  (:export
+   :load-library
+   :unload-library
+   :unlink-queue
+   :open-queue
+   :close-queue 
+   :with-open-queue
+   :send-message-raw
+   :receive-message-raw
+   :send-message
+   :receive-message
+   :f-message
+   :send-message-timed
+   :receive-message-timed
+   :flush
+   
+   :send-message-raw-timed 
+   :receive-message-raw-timed
+   :get-attributes 
+   :set-attributes
+   :non-blocking-p
+   :set-non-blocking
+   :with-unblocked))
+
+(in-package :lemmy-central)
+(add-package-local-nickname :nd :cl-naive-dom :lemmy-central)
+(add-package-local-nickname :json :com.inuoe.jzon :lemmy-central)
+(add-package-local-nickname :time :local-time :lemmy-central)
+(add-package-local-nickname :nd.abt :cl-naive-dom.abt :lemmy-central)
